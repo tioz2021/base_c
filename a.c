0bef1b72b37799e0edb2236ab1492bfe3586ed27
tioz2021 <jenya25life@gmail.com>
2023-03-14 12:29:29 +0200

    a and b prgram for judge2

--- a.c
+++ a.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main(int argc, char* argv[]) {
 	int a;
 	int b;
+	int r = 0;
 	scanf("%d", &a);
 	scanf("%d", &b);
-
-	if(a > b) {
-        printf("%d", 1);
-	}
-	else if(a < b) {
-        printf("%d", 2);
-	}
-	else {
-        printf("%d", 0);
-	}
+	r = a + b;
+	printf("%d", r);
 
 	return 0;
 }
---
+++ b.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, char* argv[]) {
+	int a;
+	int b;
+	scanf("%d", &a);
+	scanf("%d", &b);
+
+	if(a > b) {
+        printf("%d", 1);
+	}
+	else if(a < b) {
+        printf("%d", 2);
+	}
+	else {
+        printf("%d", 0);
+	}
+
+	return 0;
+}
