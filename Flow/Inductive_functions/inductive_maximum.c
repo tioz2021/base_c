ff5f84fb6d304c265ba6feb3f8d8a232b461446b
tioz2021 <jenya25life@gmail.com>
2023-03-16 08:58:43 +0200

    maximum number

--- Flow/Inductive_functions/inductive_maximum.c
+++ Flow/Inductive_functions/inductive_maximum.c
@@ -5,18 +5,19 @@ int main(int argc, char* argv[])
     int x, i = 0;
     int m = -1000000, m_i = -1;
     scanf("%d", &x);
-    while(x != 0)
+    while (x != 0)
     {
-        if(x%2 == 0)
-        {
-            if(x > m)
+        if (x%2 == 0)
+            if (x > m)
             {
                 m = x;
                 m_i = i;
             }
-        }
         i += 1;
         scanf("%d", &x);
     }
-    printf("maximum = %d\n maximum position = %d\n", m, m_i);
+    printf("maximum = %d\n"
+           "maximum position= %d\n", m, m_i);
+
+    return 0;
 }
